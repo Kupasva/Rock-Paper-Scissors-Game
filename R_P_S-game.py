91bdadaa687ca3cf1f92f51f1dbf1a264ab81ef7
Kupasva <130740583+Kupasva@users.noreply.github.com>
2023-08-17 17:48:33 +0530

    11 changes

--- R_P_S-game.py
+++ R_P_S-game.py
@@ -1,10 +1,8 @@
 from pyfiglet import figlet_format
-#from colorama import Fore, Back, Style
-from termcolor import  colored
+
 
 msg = input("enter some text")
 
 art = figlet_format(msg)
 print(art)
-colour = colored(art, 'red')
-# choice = input("write a color")
+
