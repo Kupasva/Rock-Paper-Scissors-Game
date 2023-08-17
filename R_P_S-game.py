1f6296ab8128f678b0947defbbb4f7eef2489b7a
Kupasva <130740583+Kupasva@users.noreply.github.com>
2023-08-17 17:06:05 +0530

    8 changes

--- R_P_S-game.py
+++ R_P_S-game.py
@@ -1,5 +1,15 @@
 from pyfiglet import figlet_format
 from termcolor import colored
-input = input("choose your move with rock paper, paper , scissor:")
-my_art = figlet_format(input)
-print(my_art)
+msg = input("write a word:")
+
+color = input("choose color:")
+
+valid_color = ('red', 'green', 'blue', 'cyan', 'white')
+
+if color not in valid_color:
+    color = "white"
+
+accii_art = figlet_format(msg,font='slant')
+
+color_ascii = colored(accii_art,color)
+print(color_ascii)
