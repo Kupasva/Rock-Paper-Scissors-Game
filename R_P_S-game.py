c1f6b5766333bbd38c886c39774e36b633768363
Kupasva <130740583+Kupasva@users.noreply.github.com>
2023-08-18 18:42:33 +0530

    Most Amazing project. But there is some more misunderstanding in this project. 2nd day change 2 --

--- R_P_S-game.py
+++ R_P_S-game.py
@@ -14,11 +14,21 @@ def resize_image(image, new_width = 100):
 def grayify(image):
     grayscale_image = image.convert("L")
     return(grayscale_image)
+# convert pixels to a string of ascii characters
+def pixels_to_ascii(image):
+    pixels = image.getdata()
+    characters = "".join(ascii_charc[pixels//25] for pixel in pixels)
 def main():
     path = input("enter a valid pathname to an image:\n")
     try:
         image = PIL.Image.open(path)
     except:
         print(path, " is not a valid pathname to an image.")
+
+    new_image_data = pixels_to_ascii(grayify(resize_image(image)))
+
+    # format
+    pixel_count = len(new_image_data)
+    ascii_image = "\n".join(new_image_data[i:(i+new_width)])
 
 main()
