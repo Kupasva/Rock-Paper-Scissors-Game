04911f8ef9680580608b089a95bb8c7f391849e5
Kupasva <130740583+Kupasva@users.noreply.github.com>
2023-09-06 18:43:09 +0530

    demo

--- matic.py
+++ matic.py
@@ -1,20 +1,9 @@
 from asciimatics.screen import Screen
-from asciimatics.scene import Scene
-from asciimatics.effects import Cycle, Stars
-from asciimatics.renderers import FigletText
+from time import sleep
 
-def demo(screen):
-    effects = [
-        Cycle(
-            screen,
-            FigletText("ASCIIMATICS", font='big'),
-            screen.height // 2 - 8),
-        Cycle(
-            screen,
-            FigletText("ROCKS!", font='big'),
-            screen.height // 2 + 3),
-        Stars(screen, (screen.width + screen.height) // 2)
-    ]
-    screen.play([Scene(effects, 500)])
+def clas(screen):
+    screen.print_at("Hello world!",0,0)
+    screen.refresh()
+    sleep(3)
 
-Screen.wrapper(demo)
+Screen.wrapper(clas)
