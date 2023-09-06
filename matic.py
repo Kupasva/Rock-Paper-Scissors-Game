58e28f5b5a2e6984bf805ecb6a282cc37cdc067b
Kupasva <130740583+Kupasva@users.noreply.github.com>
2023-09-06 18:50:00 +0530

    ko

--- matic.py
+++ matic.py
@@ -1,9 +1,13 @@
-from asciimatics.screen import Screen
+from asciimatics.screen import ManagedScreen
+from asciimatics.scene import Scene
+from asciimatics.effects import Cycle, Stars
+from asciimatics.renderers import FigletText
 from time import sleep
 
-def clas(screen):
-    screen.print_at("Hello world!",0,0)
+@ManagedScreen
+def demo(screen=None):
+    screen.print_at('Hello world!', 0, 0)
     screen.refresh()
-    sleep(3)
+    sleep(10)
 
-Screen.wrapper(clas)
+demo()
